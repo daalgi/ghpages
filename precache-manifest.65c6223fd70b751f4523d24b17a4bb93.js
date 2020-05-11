b7e578b53a320899b462039b4d6220f482a44326
daalgi <daalgicam@gmail.com>
2020-05-11 10:01:51 +0200

    Updates

---
+++ precache-manifest.65c6223fd70b751f4523d24b17a4bb93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ae405a20034f28674abe2988718e68",
+    "url": "/ghpages/index.html"
+  },
+  {
+    "revision": "bd2a2a105eec18220d02",
+    "url": "/ghpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "610afea2415f83830f97",
+    "url": "/ghpages/static/js/2.b3fbf164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ghpages/static/js/2.b3fbf164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd2a2a105eec18220d02",
+    "url": "/ghpages/static/js/main.5053b2c4.chunk.js"
+  },
+  {
+    "revision": "964a95937cbe636b6571",
+    "url": "/ghpages/static/js/runtime-main.b4459e6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
